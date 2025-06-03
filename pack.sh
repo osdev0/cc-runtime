893991a454788a5e9756350aa3d3cb4be66e96a6
Mintsuki <mintsuki@protonmail.com>
2025-06-03 18:08:39 +0200

    Create src directory

--- pack.sh
+++ pack.sh
@@ -2,25 +2,25 @@
 
 set -ex
 
-cat >cc-runtime.c <<'EOF'
+cat >src/cc-runtime.c <<'EOF'
 #pragma GCC diagnostic ignored "-Wunused-function"
 
 EOF
 
-cat assembly.h int_endianness.h int_types.h int_lib.h int_util.h \
-    $(find . -name '*.c' | grep -v 'cc-runtime.c' | LC_ALL=C sort) >> cc-runtime.c
+cat src/assembly.h src/int_endianness.h src/int_types.h src/int_lib.h src/int_util.h \
+    $(find src -name '*.c' | grep -v 'cc-runtime\.c' | LC_ALL=C sort) >> src/cc-runtime.c
 
-for f in *.inc; do
-    sed -e "/#include \"$f\"/{r $f" -e "d}" cc-runtime.c > cc-runtime.c.tmp
-    mv cc-runtime.c.tmp cc-runtime.c
+for f in $(cd src && echo *.inc); do
+    sed -e "/#include \"$f\"/{r src/$f" -e "d}" src/cc-runtime.c > src/cc-runtime.c.tmp
+    mv src/cc-runtime.c.tmp src/cc-runtime.c
 done
 
-grep '#include <' cc-runtime.c > saved-includes
-sed '/#include/d' cc-runtime.c > cc-runtime.c.tmp
-mv cc-runtime.c.tmp cc-runtime.c
-cat saved-includes cc-runtime.c > cc-runtime.c.tmp
-mv cc-runtime.c.tmp cc-runtime.c
+grep '#include <' src/cc-runtime.c > saved-includes
+sed '/#include/d' src/cc-runtime.c > src/cc-runtime.c.tmp
+mv src/cc-runtime.c.tmp src/cc-runtime.c
+cat saved-includes src/cc-runtime.c > src/cc-runtime.c.tmp
+mv src/cc-runtime.c.tmp src/cc-runtime.c
 rm -f saved-includes
 
-awk '{for(x=1;x<=NF;x++)if($x~/_EXTERNAL_COUNTER/){sub(/_EXTERNAL_COUNTER/,++i)}}1' cc-runtime.c > cc-runtime.c.tmp
-mv cc-runtime.c.tmp cc-runtime.c
+awk '{for(x=1;x<=NF;x++)if($x~/_EXTERNAL_COUNTER/){sub(/_EXTERNAL_COUNTER/,++i)}}1' src/cc-runtime.c > src/cc-runtime.c.tmp
+mv src/cc-runtime.c.tmp src/cc-runtime.c
